eb784732f5dfffa36dfa3ea4d0f5e3b92c8de80d
philipvox <philipvox@gmail.com>
2017-05-07 16:17:41 -0400

    Add files via upload

--- public/js/box.js
+++ public/js/box.js
@@ -148,12 +148,13 @@ function MakeSquare(bool, Tbody, elements, i) {
 }
 function allTrue(obj){
     if (obj != undefined) {
+        console.log(obj);
         var spr = [3,3,3,3]
         var tri = [2,2,2]
         var rect = [3,3,3,3,5,5]
         var keysArray = [];
         var keys = Object.keys(obj);
-        var keysSorted = Object.values(obj).sort(function(a,b){return if (obj[a] != undefined)&&(obj[b] != undefined) {obj[a]-obj[b]}})
+        var keysSorted = Object.values(obj).sort(function(a,b){if (obj[a] != undefined && obj[b] != undefined) {return obj[a]-obj[b]}});
         for (var i = 0; i < keys.length; i++) {
             var val = obj[keys[i]];
             keysArray.push(val);
